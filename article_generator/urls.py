58fdad5999725e17d42f8e217bd3db34dca7641e
Aleyna <bergamaleyna@gmail.com>
2024-08-31 17:04:29 +0300

    backend completed

--- article_generator/urls.py
+++ article_generator/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('signup', views.user_signup, name='signup'),
     path('logout', views.user_logout, name='logout'),
     path('generate-article', views.generate_article, name='generate-article'),
+    path('article-list', views.article_list, name='article-list'),
+    path('article-details/<int:pk>/', views.article_details, name='article-details'),
 ]
--- article_generator/views.py
+++ article_generator/views.py
@@ -10,15 +10,14 @@ from django.conf import settings
 import os
 import assemblyai as aai
 import openai
-from pydub import AudioSegment
+from .models import Article
+from dotenv import load_dotenv
+
+
 # Create your views here.
 @login_required
 def index(request):
     return render(request, 'index.html')
-
-def test_view():
-    print(f"MEDIA_ROOT: {settings.MEDIA_ROOT}")
-    print(f"MEDIA_URL: {settings.MEDIA_URL}")
 
 @csrf_exempt
 def generate_article(request):
@@ -32,9 +31,7 @@ def generate_article(request):
         
         #get yt title
         title = yt_title(yt_link)
-        print("basligi aldik")
         
-        test_view()
         #get transcript
         transcription = get_transcription(yt_link)
         if not transcription:
@@ -46,6 +43,13 @@ def generate_article(request):
             return JsonResponse({'error': 'Failed to generate article'}, status=500)
         
         #save article to database
+        new_article =Article.objects.create(
+            user=request.user,
+            youtube_title=title,
+            youtube_link=yt_link,
+            generated_content=article_content,
+        )
+        new_article.save()
         
         #return article as response
         return JsonResponse({'content': article_content})
@@ -59,55 +63,41 @@ def yt_title(link):
     return title
 
 def download_audio(link):
-    print("Download starting...")
 
     try:
         yt = YouTube(link)
         print(f"Video Title: {yt.title}")
 
-        # Fetch available streams for debugging
         streams = yt.streams.filter(only_audio=True)
         print(f"Available audio streams: {streams}")
         
-        # Get the first available audio stream
         video = streams.first()
 
         if not video:
             print("No audio streams available.")
             return None
 
-        # Download the audio file
         out_file = video.download(output_path=settings.MEDIA_ROOT)
         print("Audio file downloaded.")
 
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
-
-        # Convert mp4 to mp3 using pydub
-        audio = AudioSegment.from_file(out_file)
-        audio.export(new_file, format="mp3")
-        print("Conversion to mp3 complete.")
-
-        # Clean up the original mp4 file
-        os.remove(out_file)
-        print("Original mp4 file removed.")
-
+        os.rename(out_file, new_file)
         return new_file
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred while downlading audio: {e}")
         return None
 
 
 def get_transcription(link):
     try:
-        # Call the function to download and convert the audio
         audio_file = download_audio(link)
         
         if audio_file:
-            # Return the file path as a success response
-            audio_file = download_audio(link)
-            aai.settings.api_key = "fbd52160efc74801968e48bda1892107"
+            load_dotenv()
+            aai.settings.api_key = os.getenv('ASSEMBLYAI_API_KEY')
+
             
             transcriber = aai.Transcriber()
             transcript = transcriber.transcribe(audio_file)
@@ -120,11 +110,12 @@ def get_transcription(link):
         return JsonResponse({'error': str(e)}, status=500)
 
 def generate_article_from_transcription(transcription):
-    openai.api_key = "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"
+    load_dotenv()
+    openai.api_key = os.getenv('OPENAI_API_KEY')
 
     prompt = f"Based on the following transcript from a YouTube video, write a comprehensive blog article, write it based on the transcript, but dont make it look like a youtube video, make it look like a proper blog article:\n\n{transcription}\n\nArticle:"
     response =  openai.completions.create(
-        model="text-davinci-003",
+        model="gpt-3.5-turbo-0125",
         prompt =  prompt,
         max_tokens=1000,
     )
@@ -144,6 +135,17 @@ def user_login(request):
             return render(request, 'login.html', {'error_message': error_message})
 
     return render(request, 'login.html')
+
+def article_list(request):
+    article_posts = Article.objects.filter(user=request.user)
+    return render(request, "all-articles.html", {'article_posts':article_posts})
+
+def article_details(request, pk):
+    article_posts_details=Article.objects.get(id=pk)
+    if request.user == article_posts_details.user:
+        return render(request, 'article-details.html', {'article_posts_details':article_posts_details})
+    else:
+        return redirect('index')
 
 def user_signup(request):
     if request.method == 'POST':
